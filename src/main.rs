2526da1524ee3665291cb0322c5e7dd4b51ae625
Alexander Spevak <alexander.spevak@hardwario.com>
2023-11-14 14:15:00 +0100

    move kafka to different file

--- src/main.rs
+++ src/main.rs
@@ -2,60 +2,59 @@ use capnp::message::ReaderOptions;
 use capnp::serialize;
 use clickhouse::inserter::Inserter;
 use clickhouse::Client;
-use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage, MessageSets};
-use kafka::Error as KafkaError;
+use kafka::consumer::Consumer;
 use log_row::LogRow;
 use std::error::Error;
 use std::result::Result;
+use std::thread;
 use std::time::{Duration, Instant};
-use std::{thread, time as systemTime};
 
 pub mod ddl;
 pub mod http_log_capnp;
 pub mod log_row;
+pub mod producer;
 
 const TRY_AGAIN_AFTER: u64 = 10;
 //FOR SOME REASON THIS NUMBER WORKS, NOT 60
 const SAFE_TIME_CLICKHOUSE: u64 = 67;
 
-fn get_kafka_consumer() -> Result<Consumer, KafkaError> {
-    let mut counter = 0;
+async fn write_to_batch_safely(inserter: &mut Inserter<LogRow>, row: &LogRow) {
     loop {
-        match Consumer::from_hosts(vec!["localhost:9092".to_owned()])
-            .with_topic("http_log".to_string())
-            .with_fallback_offset(FetchOffset::Earliest)
-            .with_group("http_log".to_string())
-            .with_offset_storage(Some(GroupOffsetStorage::Kafka))
-            .create()
-        {
-            Ok(consumer) => return Ok(consumer),
-            Err(error) => {
-                if counter > 10 {
-                    return Err(error);
-                }
-                counter += 1;
-                println!("failed to connect to to kafka. Restarting.  {:?}", error);
-                thread::sleep(systemTime::Duration::from_secs(TRY_AGAIN_AFTER));
+        match inserter.write(row).await {
+            Ok(_) => {
+                println!("row written {:?}", row);
+                return;
+            }
+            Err(err) => {
+                println!("error writing to socket {:?}", err);
+                println!("retry in {} secs", TRY_AGAIN_AFTER);
+                thread::sleep(Duration::from_secs(TRY_AGAIN_AFTER))
             }
         }
     }
 }
 
-fn get_message_sets(consumer: &mut Consumer) -> MessageSets {
+async fn process_messages(inserter: &mut Inserter<LogRow>, consumer: &mut Consumer) {
+    let mut start = Instant::now();
+
     loop {
-        match consumer.poll() {
-            Ok(m) => {
-                if m.is_empty() {
-                    return m;
-                }
-                println!("messages fetched from kafka");
-                return m;
+        let message_sets = producer::get_message_sets(consumer);
+        for ms in message_sets.iter() {
+            for m in ms.messages() {
+                let reader = serialize::read_message(m.value, ReaderOptions::new()).unwrap();
+
+                let reader = reader
+                    .get_root::<http_log_capnp::http_log_record::Reader>()
+                    .unwrap();
+                let mut row = LogRow::new(reader).unwrap();
+                row.anonymize();
+                write_to_batch_safely(inserter, &row).await;
             }
-            Err(e) => {
-                println!("could not get messages from kafka. {:?}", e);
-                println!("retry in {} secs", TRY_AGAIN_AFTER);
-                thread::sleep(Duration::from_secs(TRY_AGAIN_AFTER))
-            }
+        }
+
+        if start.elapsed() >= Duration::from_secs(SAFE_TIME_CLICKHOUSE) {
+            commit_safely(inserter, consumer).await;
+            start = Instant::now();
         }
     }
 }
@@ -80,7 +79,6 @@ async fn commit_safely(inserter: &mut Inserter<LogRow>, consumer: &mut Consumer)
     loop {
         match consumer.commit_consumed() {
             Ok(_) => {
-                println!("messages marked as consumed in kafka");
                 break;
             }
             Err(err) => {
@@ -97,6 +95,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let client = Client::default().with_url("http://localhost:8124");
 
     client.query(ddl::CREATE_TABLE_HTTP_LOG).execute().await?;
+
     println!("http_log table created");
 
     thread::sleep(Duration::from_secs(SAFE_TIME_CLICKHOUSE));
@@ -109,28 +108,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("view created");
 
     let mut inserter = client.inserter("http_log")?;
-    let mut consumer = get_kafka_consumer()?;
-    let mut start = Instant::now();
+    let mut consumer = producer::get_kafka_consumer()?;
 
-    loop {
-        let message_sets = get_message_sets(&mut consumer);
-        for ms in message_sets.iter() {
-            for m in ms.messages() {
-                let reader = serialize::read_message(m.value, ReaderOptions::new()).unwrap();
+    process_messages(&mut inserter, &mut consumer).await;
 
-                let reader = reader
-                    .get_root::<http_log_capnp::http_log_record::Reader>()
-                    .unwrap();
-                let mut row = LogRow::new(reader).unwrap();
-                row.anonymize();
-                println!("writing message to batch {:?}", row);
-                inserter.write(&row).await?;
-            }
-        }
-
-        if start.elapsed() >= Duration::from_secs(SAFE_TIME_CLICKHOUSE) {
-            commit_safely(&mut inserter, &mut consumer).await;
-            start = Instant::now();
-        }
-    }
+    Ok(())
 }
---
+++ src/producer.rs
@@ -0,0 +1,47 @@
+use kafka::Error as KafkaError;
+
+use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage, MessageSets};
+use std::time::Duration;
+use std::{thread, time as systemTime};
+
+pub fn get_kafka_consumer() -> Result<Consumer, KafkaError> {
+    let mut counter = 0;
+    loop {
+        match Consumer::from_hosts(vec!["localhost:9092".to_owned()])
+            .with_topic("http_log".to_string())
+            .with_fallback_offset(FetchOffset::Earliest)
+            .with_group("http_log".to_string())
+            .with_offset_storage(Some(GroupOffsetStorage::Kafka))
+            .create()
+        {
+            Ok(consumer) => return Ok(consumer),
+            Err(error) => {
+                if counter > 10 {
+                    return Err(error);
+                }
+                counter += 1;
+                println!("failed to connect to to kafka. Restarting.  {:?}", error);
+                thread::sleep(systemTime::Duration::from_secs(super::TRY_AGAIN_AFTER));
+            }
+        }
+    }
+}
+
+pub fn get_message_sets(consumer: &mut Consumer) -> MessageSets {
+    loop {
+        match consumer.poll() {
+            Ok(m) => {
+                if m.is_empty() {
+                    return m;
+                }
+                println!("messages fetched from kafka");
+                return m;
+            }
+            Err(e) => {
+                println!("could not get messages from kafka. {:?}", e);
+                println!("retry in {} secs", super::TRY_AGAIN_AFTER);
+                thread::sleep(Duration::from_secs(super::TRY_AGAIN_AFTER))
+            }
+        }
+    }
+}
